e3633e6cc3a1deb4c8a5c3c918778490a669e8f8
GuyOliel <guyoliel@gmail.com>
2014-09-19 17:32:33 +0300

    Added the User Service

---
+++ www/src/prePost.services/user.service.js
@@ -0,0 +1,23 @@
+(function() {
+    //This file only represents the service that is required for the profile for now.
+  function User() {
+
+    // Some fake testing data,This will be updated when the model would apear on a server side.
+    var user = {
+        this.name = "Marty Mcfly",
+        this.profilePicture = "http://upload.wikimedia.org/wikipedia/en/d/d8/Michael_J._Fox_as_Marty_McFly_in_Back_to_the_Future,_1985.jpg",
+        this.BirthDate="November 05,1955"
+    };
+
+    return {
+      getUser: function() {
+        // Simple index lookup
+        return user;
+      }
+    }
+  }
+
+  angular.module('prePost.services')
+    .factory('User', User)
+
+}())
