23dcfe6167a3d0e5dc6cc3ed7b720bee31c5d7cc
GuyOliel <guyoliel@gmail.com>
2014-09-19 18:30:18 +0300

    Made the friends profile page look like the original profile page plan #need to fix the routing of a friends friend

--- www/src/prePost.services/friends.service.js
+++ www/src/prePost.services/friends.service.js
@@ -5,10 +5,10 @@
 
     // Some fake testing data
     var friends = [
-      { id: 0, name: 'Scruff McGruff',picture:'http://placekitten.com.s3.amazonaws.com/homepage-samples/200/286.jpg' },
-      { id: 1, name: 'G.I. Joe',picture:'http://placekitten.com.s3.amazonaws.com/homepage-samples/96/139.jpg' },
-      { id: 2, name: 'Miss Frizzle',picture:'http://placekitten.com.s3.amazonaws.com/homepage-samples/200/138.jpg' },
-      { id: 3, name: 'Ash Ketchum',picture:'http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg' }
+      { id: 0, name: 'Scruff McGruff',picture:'http://placekitten.com.s3.amazonaws.com/homepage-samples/200/286.jpg',birthDate:'February 23,1998' },
+      { id: 1, name: 'G.I. Joe',picture:'http://placekitten.com.s3.amazonaws.com/homepage-samples/96/139.jpg',birthDate:'May 1,1998' },
+      { id: 2, name: 'Miss Frizzle',picture:'http://placekitten.com.s3.amazonaws.com/homepage-samples/200/138.jpg',birthDate:'October 11,1997' },
+      { id: 3, name: 'Ash Ketchum',picture:'http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg',birthDate:'July 22,1998' }
     ];
 
     return {
